f6e690b556a686bebbcb10fc3be49c6cf8d8bf64
SharonJseg <sharon.j.segal@gmail.com>
2022-02-25 00:57:10 +0200

    fixed style names

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,29 +4,29 @@ import orderIcon from '../../images/icon-music.svg';
 
 const Card = () => {
   return (
-    <div className={classes.card}>
+    <div className={classes._container}>
       <figure>
         <img
           src={cardImage}
           alt='card illustration'
-          className={classes.card__image}
+          className={classes._image}
         />
       </figure>
-      <h2 className={classes.card__title}>Order Summary</h2>
-      <p className={classes.card__text}>
+      <h2 className={classes._title}>Order Summary</h2>
+      <p className={classes._text}>
         You can now listen to millions of songs, audiobooks, and podcasts on any
         device anywhere you like!
       </p>
-      <a href='#' className={classes.order__container}>
-        <img src={orderIcon} alt='icon music' className={classes.order__icon} />
-        <div className={classes.order__details}>
-          <p className={classes.order__plan}>Annual Plan</p>
-          <p className={classes.order__price}>$59.99/year</p>
+      <a href='#' className={classes._order_container}>
+        <img src={orderIcon} alt='icon music' className={classes._order_icon} />
+        <div className={classes._order_details}>
+          <p className={classes._order_plan}>Annual Plan</p>
+          <p className={classes._order_price}>$59.99/year</p>
         </div>
-        <button className={classes.order__change}>Change</button>
+        <button className={classes._order_change}>Change</button>
       </a>
-      <button className={classes.card__paybutton}>Proceed to Payment</button>
-      <button className={classes.card__cancel}>Cancel Order</button>
+      <button className={classes._paybutton}>Proceed to Payment</button>
+      <button className={classes._cancel}>Cancel Order</button>
     </div>
   );
 };
